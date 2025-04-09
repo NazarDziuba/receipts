0467226d5259283c48f75a65a678cf5fd5f78a6d
Nazar Dziuba <ndzuba455@gmail.com>
2025-04-09 21:17:03 +0300

    added fetch and fetched list img and categories

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const menuPhoneOpened = document.querySelector('#menuPhoneOpened')
 const menuIcon = document.querySelector('#menuIcon')
 const menuClose = document.querySelector('#menuClose')
 const searchField = document.querySelector('#searchFieldDiv')
-const searchBtn = document.querySelector('#search')
+const searchBtnPhone = document.querySelector('#search')
+const closeSearchBtn = document.getElementById('menuCloseBigDisplay')
+const searchImg = document.getElementById('searchImg')
+const url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?tags=vegetarian,dessert&number=20"
 
 if(window.innerWidth < 800){
     headerBig.style.display = "none";
@@ -21,20 +24,117 @@ else{
     
 }
 
-menuIcon.onclick = menuButtonClick
-menuClose.onclick = menuCloseButton
-searchBtn.onclick = searchBtnClick
+menuIcon.onclick = menuButtonClickSmallDisplays
+menuClose.onclick = menuCloseButtonSmallDisplays
+searchImg.addEventListener("click", searchBtnClick)
 
-function menuButtonClick(){
+function menuButtonClickSmallDisplays(){
     menuIcon.style.display = "none";
     menuPhoneOpened.style.display = "grid";
 }
-function searchBtnClick(){
-    searchField.style.display = "grid";
-    /*searchBtn.style.display = "none";*/
-}
-function menuCloseButton(){
+
+function menuCloseButtonSmallDisplays(){
     menuPhoneOpened.style.display = "none";
     menuIcon.style.display = "block";
     searchField.style.display = "none";
 }
+
+function searchBtnClick(){
+    const searchDiv = document.getElementById('searchDiv');
+    searchDiv.style.display = "flex";
+    searchImg.style.display = "none";
+    /*searchBtn.style.display = "none";*/
+}
+    
+closeSearchBtn.addEventListener("click", () => {
+    const searchDiv = document.getElementById('searchDiv')
+    searchDiv.style.display = "none";
+    searchImg.style.display = "flex";
+});
+
+let recipeData = null;
+
+const fetchData = async () => {
+
+    try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key": "fce02e8d37msh974938717a83848p1f571djsn14117f60c9f0"
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error(`Ошибка: ${response.status}`);
+        }
+
+        const data = await response.json();
+        recipeData = data.recipes;
+        //console.log(recipeData);
+        listImgFetch(recipeData);
+        categoryNameFetch(recipeData);
+      } 
+
+    catch (error) {
+        console.error("Ошибка при получении рецепта:", error);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", fetchData);
+
+const listImgFetch = (recipeData) => {
+  const savedData = localStorage.getItem("recipeData");
+
+  if(savedData){
+    recipeData = JSON.parse(savedData);
+    console.log("Загружено: ", recipeData);
+    showImages(recipeData);
+  }
+
+  else{
+    localStorage.setItem("recipeData", JSON.stringify(recipeData));
+    showImages(recipeData);
+  }
+};
+
+const showImages = (recipes) => {
+    const listImgArr = Array.from(document.querySelectorAll('.listImg'));
+    recipes.sort((a, b) => b.aggregateLikes - a.aggregateLikes );
+    
+    listImgArr.forEach((img, index) => {
+        if (recipes[index]) {
+          img.src = recipes[index].image;
+          console.log(`Index: ${index}; Likes: ${recipes[index].aggregateLikes};`);
+        }
+      })
+};
+
+const categoryNameFetch = (recipes) => {
+
+   const categoryUl = document.querySelector(".categoryUl");
+   for (let i = 1; i <= 20; i++){
+   const newLi = document.createElement("li");
+   categoryUl.appendChild(newLi);
+   }
+   const alLi = categoryUl.querySelectorAll("li")
+   alLi.forEach((li, index) => {
+
+    if(recipes[index]){
+        li.textContent = recipes[index].title;
+        //console.log(recipes[index].title)
+    }
+
+   })
+
+   
+   
+   
+   
+    /*const recipeTitle = recipes.title;
+    console.log(recipeTitle);*/
+
+}
+
+
+
